0e67f4148a3daa8ed8f92cccf9ddf535eac13809
Eric MORAND <eric.morand@gmail.com>
2018-02-09 19:11:46 +0100

    Fix *module* node unit test

--- test/environment-stub.ts
+++ test/environment-stub.ts
@@ -1,11 +1,14 @@
 import TwingEnvironment from "../src/environment";
-import TwingLoaderArray from "../src/loader/array";
 import TwingTestLoaderStub from "./loader-stub";
 import TwingEnvironmentOptions from "../src/environment-options";
 
 class TwingTestEnvironmentStub extends TwingEnvironment {
     constructor(options: TwingEnvironmentOptions = {}) {
         super(new TwingTestLoaderStub(), options);
+    }
+
+    getTemplateClass(name: string, index: number = null) {
+        return '__TwingTemplate_foo';
     }
 }
 
