52d793df11a73fddd8cd7133baaf8332dd783430
Eric MORAND <eric.morand@gmail.com>
2019-12-16 14:26:38 +0100

    Fix issue #466

--- src/lib/extension/core/functions/max.ts
+++ src/lib/extension/core/functions/max.ts
@@ -1,15 +1,10 @@
 import {iteratorToArray} from "../../../helpers/iterator-to-array";
+import {max as phpMax} from "locutus/php/math";
 
-export function max(...values: any[]): Promise<any> {
+export function max(...values: Array<any>): Promise<any> {
     if (values.length === 1) {
         values = values[0];
     }
 
-    let array = iteratorToArray(values);
-
-    array.sort((a: any, b: any) => {
-        return a < b ? 1 : -1;
-    });
-
-    return Promise.resolve(array[0]);
+    return Promise.resolve(phpMax(iteratorToArray(values)));
 }
--- test/tests/unit/lib/extension/core/functions/max/test.ts
+++ test/tests/unit/lib/extension/core/functions/max/test.ts
@@ -5,6 +5,12 @@ tape('max', async (test) => {
     test.same(await max(1, 3, 2), 3);
     test.same(await max('foo', 'bar'), 'foo');
     test.same(await max({foo: 'foo', bar: 'bar'}), 'foo');
+    test.same(await max([1, 3, 2]), 3);
+    test.same(await max(0, 'a', 2), 2);
+    test.same(await max('b', 'a'), 'b');
+    test.same(await max('b', 'a', 2), 2);
+    test.same(await max('b', 'a', -2), 'b');
+    test.same(await max(-5, 5, 10, -10, 20, -20), 20);
 
     test.end();
 });
