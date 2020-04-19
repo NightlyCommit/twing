87c2ce48b4a36ea9ccac3e9caf70a704bffd0469
Eric MORAND <eric.morand@gmail.com>
2020-04-19 21:44:58 +0200

    Fix issue #486

--- src/lib/extension/core/filters/merge.ts
+++ src/lib/extension/core/filters/merge.ts
@@ -2,6 +2,7 @@ import {merge as mergeHelper} from "../../../helpers/merge";
 import {isNullOrUndefined} from "util";
 import {isTraversable} from "../../../helpers/is-traversable";
 import {TwingErrorRuntime} from "../../../error/runtime";
+import {iteratorToMap} from "../../../helpers/iterator-to-map";
 
 /**
  * Merges an array with another one.
@@ -14,12 +15,12 @@ import {TwingErrorRuntime} from "../../../error/runtime";
  *  {# items now contains { 'apple': 'fruit', 'orange': 'fruit', 'peugeot': 'car' } #}
  * </pre>
  *
- * @param {Map<any, any>} iterable1 A map
- * @param {Map<any, any>} iterable2 A map
+ * @param {any} iterable1 An iterable
+ * @param {any} iterable2 An iterable
  *
  * @return {Promise<Map<any, any>>} The merged map
  */
-export function merge(iterable1: Map<any, any>, iterable2: Map<any, any>): Promise<Map<any, any>> {
+export function merge(iterable1: any, iterable2: any): Promise<Map<any, any>> {
     if (isNullOrUndefined(iterable1) || (!isTraversable(iterable1) && (typeof iterable1 !== 'object'))) {
         throw new TwingErrorRuntime(`The merge filter only works with arrays or "Traversable", got "${!isNullOrUndefined(iterable1) ? typeof iterable1 : iterable1}" as first argument.`);
     }
@@ -28,5 +29,5 @@ export function merge(iterable1: Map<any, any>, iterable2: Map<any, any>): Promi
         throw new TwingErrorRuntime(`The merge filter only works with arrays or "Traversable", got "${!isNullOrUndefined(iterable2) ? typeof iterable2 : iterable2}" as second argument.`);
     }
 
-    return Promise.resolve(mergeHelper(iterable1, iterable2));
+    return Promise.resolve(mergeHelper(iteratorToMap(iterable1), iteratorToMap(iterable2)));
 }
