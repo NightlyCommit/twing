bec8498bd9af3eb6d7e3eab574ba9f5503682cc9
Michael Warner <mike@insidenewcity.com>
2023-04-10 08:47:40 +0000

    resolve #552 and add some corresponding 'iterable' tests

--- src/lib/extension/core/tests/iterable.ts
+++ src/lib/extension/core/tests/iterable.ts
@@ -14,6 +14,27 @@
  */
 export function iterable(value: any): Promise<boolean> {
     let _do = (): boolean => {
+        /*
+            Prevent `(null)[Symbol.iterator]`/`(undefined)[Symbol.iterator]` error,
+            and return `false` instead.
+
+            Note that `value` should only be `undefined` if it's been explicitly
+            set to that (e.g., in the JavaScript that provided the calling template
+            with the context). Values that are simply "not defined" will either have
+            been coerced to `null` or thrown a "does not exist" runtime error before
+            this function is called (depending on whether `strict_variables` is enabled).
+
+            This *does* mean that an explicitly `undefined` value will return `false`
+            instead of throwing an error if `strict_variables` is enabled, which is
+            probably unexpected behavior, but short of some major refactoring to allow
+            an environmental check here, the alternative is to have `undefined`
+            throw an error even when `strict_variables` is disabled, and that unexpected
+            behavior seems worse.
+        */
+        if (value === null || value === undefined) {
+            return false;
+        }
+
         // for Twig, a string is not traversable
         if (typeof value === 'string') {
             return false;
